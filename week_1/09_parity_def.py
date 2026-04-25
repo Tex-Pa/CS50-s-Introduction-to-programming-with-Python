c5c739acb8a6135525c5bff7f4ca115be5ac9292
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 10:57:19 +0000

    Sat, Apr 25, 2026, 12:57 PM +02:00

--- week_1/09_parity_def.py
+++ week_1/09_parity_def.py
@@ -1,8 +1,22 @@
 #definisco una funzione e la chiamo main
-def main()
+def main():
     #qui dico all'interno della funzione che x che è parte della funzione deve essere intero e deve essere inserito dall'user
     x = int(input("What's x? "))
-    
+    #qui devo inventare un'altra funzione is_even e sarà la funzione che mi permetterà di di elaborare le condizioni
+    #qui l'unica cosa che non capisco è la x tra le parentesi
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+#qui andiamo a definire la funzione is_even che fara i confronti ma ancora non capisco cosa significa la n tra parentesi, cosa indica e perchè si mette
+def is_even(n):
+    if n % 2 == 0:
+        
+
+
+
+
 
 
 
