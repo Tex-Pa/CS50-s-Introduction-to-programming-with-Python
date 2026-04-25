2d0aa56a6efe3530e278e1595032ad0fe13a636e
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 10:13:28 +0000

    Sat, Apr 25, 2026, 12:13 PM +02:00

---
+++ week_1/06_not_equal.py
@@ -0,0 +1,12 @@
+#qui nominiamo 2 variabili e chiediamo di scrivere due numeri interi ad un operatore
+
+x = int(input("What's x? "))
+y = int(input("what's y? "))
+
+if x != y :
+    print("x is not equal than y")
+
+else:
+    print("x is equal than y")
+
+
