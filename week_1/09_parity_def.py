10dd6d37ba226115ca1ed281d482f99ba897039a
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 11:13:05 +0000

    Sat, Apr 25, 2026, 1:13 PM +02:00

--- week_1/09_parity_def.py
+++ week_1/09_parity_def.py
@@ -3,13 +3,14 @@ def main():
     #qui dico all'interno della funzione che x  è parte della funzione deve essere intero e deve essere inserito dall'user
     x = int(input("What's x? "))
     #qui devo inventare un'altra funzione is_even e sarà la funzione che mi permetterà di elaborare le condizioni
-    #dico di assegnare a is_even il valore di x 
+    #dico di assegnare a is_even il valore di x e di scrivere Even in funzione di una condizione if che abbiamo visto sotto
     if is_even(x):
         print("Even")
     else:
         print("Odd")
 
-#qui andiamo a definire la funzione is_even che fara i confronti ma ancora non capisco cosa significa la n tra parentesi, cosa indica e perchè si mette
+#qui andiamo a definire la funzione is_even che fara i confronti e diciamo che is_even è sicuramente un numero che ancora non conosciamo ma grazie al codice precedente sappiamo che sarà uguale a x
+#e gli chiediamo di verificare queste condizioni
 def is_even(n):
     if n % 2 == 0:
         return True
