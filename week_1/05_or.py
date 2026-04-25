1b6c4f6ea6a23270758f3260021c5cad42cc6032
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 10:06:24 +0000

    Sat, Apr 25, 2026, 12:06 PM +02:00

---
+++ week_1/05_or.py
@@ -0,0 +1,14 @@
+#qui nominiamo 2 variabili e chiediamo di scrivere due numeri interi ad un operatore
+
+x = int(input("What's x? "))
+y = int(input("what's y? "))
+
+#qui vogliamo confrontare i 2 numeri interi e farli comparare a Python
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else :
+    print("x is equal to y")
