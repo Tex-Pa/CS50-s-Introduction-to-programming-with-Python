6146a0d1265b89dde500dc4f5fa94bdbe7fba858
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 10:50:09 +0000

    Sat, Apr 25, 2026, 12:50 PM +02:00

--- week_1/09_parity_def.py
+++ week_1/09_parity_def.py
@@ -1,4 +1,8 @@
+#definisco una funzione e la chiamo main
 def main()
+    #qui dico all'interno della funzione che x che è parte della funzione deve essere intero e deve essere inserito dall'user
+    x = int(input("What's x? "))
     
+
 
 
