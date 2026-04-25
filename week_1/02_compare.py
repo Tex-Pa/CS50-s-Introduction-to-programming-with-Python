9c098b48c0a2f17b5d12fa4c78e0d0028aafe097
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 09:20:44 +0000

    Sat, Apr 25, 2026, 11:20 AM +02:00

---
+++ week_1/02_compare.py
@@ -0,0 +1,10 @@
+#Simbols for condicionals
+#> maggiore di
+#=> maggiore uguale di
+#< minore di
+#<= minore uguale di
+#== uguale a
+#!= non è uguale a
+
+#if serve per chiedere sostituisce il significato di se...
+#
