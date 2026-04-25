ad14e5a9839f3f69b9034a96d86c238f04bad2c6
Tex-Pa <cristianbarrale@yahoo.com>
2026-04-25 10:08:51 +0000

    Sat, Apr 25, 2026, 12:08 PM +02:00

--- week_1/05_or.py
+++ week_1/05_or.py
@@ -4,11 +4,10 @@ x = int(input("What's x? "))
 y = int(input("what's y? "))
 
 #qui vogliamo confrontare i 2 numeri interi e farli comparare a Python
-if x < y:
-    print("x is less than y")
+if x < y or x > y:
+    print("x is not equal than y")
 
-elif x > y:
-    print("x is greater than y")
+else:
+    print("x is equal than y")
 
-else :
-    print("x is equal to y")
+
